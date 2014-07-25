d443afa8f3f0992981dd2c0e1695a4a6e1a94efb
Tughi <admin@tughi.com>
2014-07-25 16:30:23 +0200

    drawer menu

--- app/src/main/java/com/tughi/aggregator/ui/MainActivity.java
+++ app/src/main/java/com/tughi/aggregator/ui/MainActivity.java
@@ -3,6 +3,7 @@ package com.tughi.aggregator.ui;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.LoaderManager;
+import android.content.Context;
 import android.content.CursorLoader;
 import android.content.Loader;
 import android.content.res.Configuration;
@@ -13,10 +14,12 @@ import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.CursorAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 import com.tughi.aggregator.R;
 import com.tughi.aggregator.content.AggregatorUris;
@@ -61,14 +64,7 @@ public class MainActivity extends Activity implements LoaderManager.LoaderCallba
         };
         drawerLayout.setDrawerListener(drawerToggle);
 
-        drawerListAdapter = new SimpleCursorAdapter(
-                this,
-                R.layout.drawer_feed_item,
-                null,
-                new String[]{"title", "unread_count"},
-                new int[]{R.id.title, R.id.unread_count},
-                0
-        );
+        drawerListAdapter = new FeedListAdapter();
 
         drawerListView = (ListView) drawerLayout.findViewById(R.id.drawer);
         drawerListView.setAdapter(drawerListAdapter);
@@ -80,8 +76,6 @@ public class MainActivity extends Activity implements LoaderManager.LoaderCallba
         getActionBar().setHomeButtonEnabled(true);
 
         title = getTitle();
-
-        selectDrawerItem(0, 1);
     }
 
     @Override
@@ -99,9 +93,18 @@ public class MainActivity extends Activity implements LoaderManager.LoaderCallba
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.drawer, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
         boolean drawerOpen = drawerLayout.isDrawerOpen(drawerListView);
-        getFragmentManager().findFragmentById(R.id.content).setHasOptionsMenu(!drawerOpen);
+
+        menu.setGroupVisible(R.id.content, !drawerOpen);
+        menu.setGroupVisible(R.id.drawer, drawerOpen);
 
         return super.onPrepareOptionsMenu(menu);
     }
@@ -137,25 +140,95 @@ public class MainActivity extends Activity implements LoaderManager.LoaderCallba
         getFragmentManager()
                 .beginTransaction()
                 .replace(R.id.content, fragment)
-                .commit();
+                .commitAllowingStateLoss();
 
+        // mark position as active
         drawerListView.setItemChecked(position, true);
+
+        // finish
         drawerLayout.closeDrawer(drawerListView);
     }
 
+    private static final String[] FEED_PROJECTION = {
+            "_id",
+            "title",
+            "unread_count"
+    };
+    private static final int FEED_ID_INDEX = 0;
+    private static final int FEED_TITLE_INDEX = 1;
+    private static final int FEED_UNREAD_COUNT_INDEX = 2;
+
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return new CursorLoader(this, AggregatorUris.newFeedsUri(), null, null, null, null);
+        return new CursorLoader(this, AggregatorUris.newFeedsUri(), FEED_PROJECTION, null, null, null);
     }
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
         drawerListAdapter.swapCursor(cursor);
+
+        if (!isFinishing() && getFragmentManager().findFragmentById(R.id.content) == null) {
+            // add default fragment
+            selectDrawerItem(0, -1);
+        }
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
         drawerListAdapter.swapCursor(null);
+    }
+
+    /**
+     * A {@link ListAdapter} for the drawer view.
+     */
+    private class FeedListAdapter extends CursorAdapter {
+
+        public FeedListAdapter() {
+            super(MainActivity.this, null, false);
+        }
+
+        @Override
+        public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+            View view = getLayoutInflater().inflate(R.layout.drawer_feed_item, viewGroup, false);
+
+            ViewTag tag = new ViewTag();
+            tag.titleTextView = (TextView) view.findViewById(R.id.title);
+            tag.unreadCountTextView = (TextView) view.findViewById(R.id.unread_count);
+            view.setTag(tag);
+
+            return view;
+        }
+
+        @Override
+        public void bindView(View view, Context context, Cursor cursor) {
+            ViewTag tag = (ViewTag) view.getTag();
+
+            // set feed title
+            switch (cursor.getInt(FEED_ID_INDEX)) {
+                case -1:
+                    tag.titleTextView.setText(R.string.unread_feed);
+                    break;
+                case -2:
+                    tag.titleTextView.setText(R.string.starred_feed);
+                    break;
+                default:
+                    tag.titleTextView.setText(cursor.getString(FEED_TITLE_INDEX));
+            }
+
+            // set feed unread items count
+            if (cursor.getInt(FEED_UNREAD_COUNT_INDEX) > 0) {
+                tag.unreadCountTextView.setText(cursor.getString(FEED_UNREAD_COUNT_INDEX));
+                tag.unreadCountTextView.setVisibility(View.VISIBLE);
+            } else {
+                tag.unreadCountTextView.setVisibility(View.GONE);
+            }
+        }
+
+        private class ViewTag {
+            private TextView titleTextView;
+            private TextView unreadCountTextView;
+        }
+
     }
 
 }
