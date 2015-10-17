a5d9673c98f4b17ced8a9d54b6566c2bf1192610
tughi <admin@tughi.com>
2015-10-17 14:41:45 +0200

    rss generator

--- generator.py
+++ generator.py
@@ -1,4 +1,5 @@
 import codecs
+import dateutil.parser
 import jinja2
 import os
 import markdown
@@ -7,11 +8,14 @@ import shutil
 SOURCES = {}
 JINJA2 = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
 TEMPLATES = {
-    'howto': JINJA2.get_template('howto.jinja2')
+    'howto': JINJA2.get_template('howto.jinja2'),
+    'news': JINJA2.get_template('news.jinja2'),
 }
 
 
 def process_content():
+    news = []
+
     os.chdir('content')
     for root, dirs, files in os.walk('./'):
         if root.startswith('./'):
@@ -22,19 +26,23 @@ def process_content():
         except OSError:
             pass
 
-        for file_name in files:
+        for file_name in sorted(files):
             file_path = os.path.join(root, file_name)
 
             if file_name.endswith('.source'):
                 source = load_source(file_path)
 
                 source_type = source['type']
-                if source_type != 'news':
+                if source_type == 'news':
+                    news.append(source)
+                else:
                     generate_html(source)
-                pass
+
             else:
                 print 'copying', file_path
                 shutil.copy(file_path, os.path.join('../gh-pages', file_path))
+
+    generate_rss(news)
 
 
 def load_source(file_path):
@@ -43,7 +51,7 @@ def load_source(file_path):
 
         with codecs.open(file_path, mode='r', encoding='utf-8') as reader:
             source = SOURCES[file_path] = {
-                'source': file_path
+                'file': file_path
             }
 
             for metadata in reader:
@@ -54,7 +62,10 @@ def load_source(file_path):
                 name = name.strip()
                 value = value.strip()
 
-                source[name] = value
+                if name == 'date':
+                    source[name] = dateutil.parser.parse(value)
+                else:
+                    source[name] = value
 
             source_content = reader.read()
             if source_content:
@@ -65,14 +76,32 @@ def load_source(file_path):
 
 def generate_html(source):
     source_type = source['type']
-    source_source = source['source']
+    source_file = source['file']
 
-    print 'generating', source_type, 'from', source_source
+    print 'generating', source_type, 'from', source_file
 
     template = TEMPLATES[source_type]
 
-    with codecs.open(os.path.join('../gh-pages', source_source[:-6] + 'html'), mode='w', encoding='utf-8') as writer:
+    with codecs.open(os.path.join('../gh-pages', source_file[:-6] + 'html'), mode='w', encoding='utf-8') as writer:
         writer.write(template.render(**source))
+
+
+def generate_rss(news):
+    if not news:
+        return
+
+    print 'generating rss news'
+
+    template = TEMPLATES['news']
+
+    for item in news:
+        if 'page' in item:
+            page_source = SOURCES[item['page']].copy()
+            page_source.update(item)
+            item.update(page_source)
+
+    with codecs.open(os.path.join('../gh-pages', 'news.rss'), mode='w', encoding='utf-8') as writer:
+        writer.write(template.render({'news': news}))
 
 
 if __name__ == '__main__':
